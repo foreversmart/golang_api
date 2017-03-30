f294f5bc7d3d9d56a73bdb54680c4592e3610e4a
rorschach <njutree@gmail.com>
2017-03-30 19:20:11 +0800

    add packages cache

--- package.go
+++ package.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"craw"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
-	"qbox.us/encoding/json"
 	"strings"
 	"workflow"
 )
@@ -16,7 +17,25 @@ func main() {
 	flag.StringVar(&input, "input", "", "query")
 	flag.Parse()
 
-	pkgs, descs := craw.GetPkgsAndDesc("https://golang.org/pkg/")
+	data, err := readCache()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	var pkgs, descs []string
+	if data == nil {
+		pkgs, descs = craw.GetPkgsAndDesc("https://golang.org/pkg/")
+		data = &Data{
+			Pkgs:  pkgs,
+			Descs: descs,
+		}
+
+		data.Flush()
+	} else {
+		pkgs = data.Pkgs
+		descs = data.Descs
+	}
 
 	res := &workflow.Content{
 		Items: make([]*workflow.Item, 0, 5),
@@ -37,4 +56,49 @@ func main() {
 	fmt.Println(res.Str())
 }
 
+func readCache() (data *Data, err error) {
+	path, err := cachePath()
+	if err != nil {
+		return data, err
+	}
 
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return data, err
+	}
+
+	if len(content) == 0 {
+		return
+	}
+
+	err = json.Unmarshal(content, &data)
+	return
+}
+
+type Data struct {
+	Pkgs  []string `json:"pkgs"`
+	Descs []string `json:"descs"`
+}
+
+func (data *Data) Flush() error {
+	path, err := cachePath()
+	if err != nil {
+		return err
+	}
+
+	content, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(path, content, 0666)
+}
+
+func cachePath() (path string, err error) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	return pwd + "/.cache", nil
+}
