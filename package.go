e7647502dcfe898ee165e3e8a1d142adbf0b2b86
rorschach <njutree@gmail.com>
2017-05-01 10:20:36 +0800

    add package support member

--- package.go
+++ package.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"match"
 	"os"
 	"strings"
 	"workflow"
@@ -14,15 +15,27 @@ import (
 
 func main() {
 	var input string
+	var member string
+	var isMember bool
 	flag.StringVar(&input, "input", "", "query")
 	flag.Parse()
+	input = strings.Replace(input, "  ", " ", -1)
+	input = strings.TrimSpace(input)
+	inputs := strings.Split(input, " ")
+	if len(inputs) > 1 {
+		isMember = true
+		member = inputs[1]
+		input = inputs[0]
+	}
 
+	// read cache
 	data, err := readCache()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
+	// recover cache
 	var pkgs, descs []string
 	if data == nil {
 		pkgs, descs = craw.GetPkgsAndDesc("https://golang.org/pkg/")
@@ -41,16 +54,47 @@ func main() {
 		Items: make([]*workflow.Item, 0, 5),
 	}
 
-	for i, pkg := range pkgs {
-		if strings.HasPrefix(pkg, input) {
+	matchPkgs := match.MatchArr(input, pkgs)
+
+	// deal member
+	if isMember {
+		links, members, descs := craw.CrawPkgMembers("https://golang.org/pkg/" + matchPkgs[0])
+
+		// make reverse map
+		reverseMap := make(map[string]int)
+		for i, value := range members {
+			reverseMap[value] = i
+		}
+
+		matchMembers := match.MatchArr(member, members)
+
+		for _, value := range matchMembers {
 			res.Items = append(res.Items, &workflow.Item{
-				Title:    pkg,
+				Title:    value,
 				Valid:    "yes",
-				SubTitle: descs[i],
+				SubTitle: descs[reverseMap[value]],
 				Icon:     "icon.png",
-				Arg:      pkg,
+				Arg:      matchPkgs[0] + "/" + links[reverseMap[value]],
 			})
 		}
+
+		fmt.Println(res.Str())
+		return
+	}
+
+	pkgMap := make(map[string]string)
+	for i, pkg := range pkgs {
+		pkgMap[pkg] = descs[i]
+	}
+
+	for _, pkg := range matchPkgs {
+		res.Items = append(res.Items, &workflow.Item{
+			Title:    pkg,
+			Valid:    "yes",
+			SubTitle: pkgMap[pkg],
+			Icon:     "icon.png",
+			Arg:      pkg,
+		})
 	}
 
 	fmt.Println(res.Str())
